797c83a38cc3c6b2738421eaed25a12d59c42fff
Stephanie Tuerk <stephanie.tuerk@gmail.com>
2025-11-02 12:34:40 -0500

    fix: update tags endpoint for updated prisma version

--- src/services/tag.service.ts
+++ src/services/tag.service.ts
@@ -1,36 +1,18 @@
 import prisma from '../prisma/prisma-client';
 
 const getTags = async (username?: string): Promise<string[]> => {
-  const queries = [];
+  const where = username
+    ? { articles: { some: { author: { username } } } }
+    : { articles: { some: {} } };
 
-  if (username) {
-    queries.push({
-      username: {
-        equals: username,
-      },
-    });
-  }
-
-  const tags = await prisma.tag.groupBy({
-    where: {
-      articles: {
-        some: {
-          author: {
-            OR: queries,
-          },
-        },
-      },
-    },
-    by: ['name'],
-    orderBy: {
-      _count: {
-        name: 'desc',
-      },
-    },
+  const tags = await prisma.tag.findMany({
+    where,
+    select: { name: true, _count: { select: { articles: true } } },
+    orderBy: { articles: { _count: 'desc' } },
     take: 10,
   });
 
-  return tags.map(tag => tag.name);
+  return tags.map(t => t.name);
 };
 
 export default getTags;
